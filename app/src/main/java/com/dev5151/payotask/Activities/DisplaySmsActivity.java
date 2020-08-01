52fc9cee49c4a8a121a7771758b7cd9bb616cf27
dev5151 <iamdev5151@gmail.com>
2020-08-02 00:31:56 +0530

    Added Google Sign In

--- app/src/main/java/com/dev5151/payotask/Activities/DisplaySmsActivity.java
+++ app/src/main/java/com/dev5151/payotask/Activities/DisplaySmsActivity.java
@@ -14,6 +14,7 @@ import com.dev5151.payotask.Adapters.SmsAdapter;
 import com.dev5151.payotask.Models.Sms;
 import com.dev5151.payotask.R;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,6 +34,7 @@ public class DisplaySmsActivity extends AppCompatActivity {
     Integer income = 0, expense = 0;
     String amtString = null;
     DatabaseReference messageRef;
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -41,6 +43,7 @@ public class DisplaySmsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_sms);
 
         recyclerView = findViewById(R.id.recycler_view);
+        mAuth=FirebaseAuth.getInstance();
         messageRef = FirebaseDatabase.getInstance().getReference().child("sms");
         extendedFloatingActionButton = findViewById(R.id.fab);
         smsArrayList = fetchList();
--- app/src/main/java/com/dev5151/payotask/Activities/ReadSmsActivity.java
+++ app/src/main/java/com/dev5151/payotask/Activities/ReadSmsActivity.java
@@ -10,12 +10,26 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.dev5151.payotask.Models.Sms;
 import com.dev5151.payotask.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,17 +39,25 @@ import com.google.firebase.database.ValueEventListener;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ReadSmsActivity extends AppCompatActivity {
 
+    private static final String TAG = "auth";
     Button fetchMessage;
     List<Sms> smsList = new ArrayList<>();
     DatabaseReference messageRef;
+    private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_s_m_s);
+
+        initViews();
 
         fetchMessage = findViewById(R.id.fetch_message);
         messageRef = FirebaseDatabase.getInstance().getReference().child("sms");
@@ -43,25 +65,32 @@ public class ReadSmsActivity extends AppCompatActivity {
         fetchMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (checkSelfPermission(Manifest.permission.READ_SMS)!= PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.READ_SMS},1);
-                    return;
-                }else{
-                    Intent intent = new Intent(getApplicationContext(), DisplaySmsActivity.class);
-                    Bundle args = new Bundle();
-                    smsList = getAllSms();
-                    pushToServer(smsList);
-                    args.putSerializable("ARRAYLIST", (Serializable) smsList);
-                    intent.putExtra("BUNDLE", args);
-                    startActivity(intent);
-                    finish();
+                if (mAuth.getCurrentUser() != null) {
+                    fetchMessageList();
+                } else {
+                    signIn();
                 }
-
             }
         });
 
     }
+
+    private void fetchMessageList() {
+        if (checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, 1);
+            return;
+        } else {
+            Intent intent = new Intent(getApplicationContext(), DisplaySmsActivity.class);
+            Bundle args = new Bundle();
+            smsList = getAllSms();
+            pushToServer(smsList);
+            args.putSerializable("ARRAYLIST", (Serializable) smsList);
+            intent.putExtra("BUNDLE", args);
+            startActivity(intent);
+            finish();
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
@@ -79,7 +108,7 @@ public class ReadSmsActivity extends AppCompatActivity {
 
                 } else {
 
-                    Toast.makeText(getApplicationContext(),"Access to read messages needed",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Access to read messages needed", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
@@ -88,6 +117,63 @@ public class ReadSmsActivity extends AppCompatActivity {
             // permissions this app might request
         }
     }
+
+    private void initViews() {
+
+        mAuth = FirebaseAuth.getInstance();
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            fetchMessageList();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(ReadSmsActivity.this, "" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 
     public List<Sms> getAllSms() {
         List<Sms> lstSms = new ArrayList<Sms>();
@@ -136,8 +222,8 @@ public class ReadSmsActivity extends AppCompatActivity {
 
     private void pushToServer(final List<Sms> messageList) {
         final List<Sms> newList = new ArrayList<>();
-        final List<Sms>list=new ArrayList<>();
-        messageRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        final List<Sms> list = new ArrayList<>();
+        messageRef.child(Objects.requireNonNull(mAuth.getUid())).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 list.clear();
